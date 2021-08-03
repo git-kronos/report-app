6dc07d7621c96371a1b21fbc16233885639c9cad
Shuvam Das <github.shuvam@gmail.com>
2021-08-03 22:07:13 +0530

    first commit

---
+++ apps/sales/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from apps.sales import views
+
+app_name = 'sales'
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+    path('list/', views.SaleListView.as_view(), name='list'),
+    path('<pk>/', views.SaleDetailView.as_view(), name='detail'),
+]
